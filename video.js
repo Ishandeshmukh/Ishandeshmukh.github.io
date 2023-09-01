9acacb33953058256517b053c98cc45a7faef281
Ishandeshmukh <141939822+Ishandeshmukh@users.noreply.github.com>
2023-09-01 14:54:34 +0530

    Update video.js

--- video.js
+++ video.js
@@ -101,3 +101,9 @@ document.getElementById('join-btn').addEventListener('click', joinStream)
 document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
 document.getElementById('mic-btn').addEventListener('click', toggleMic)
 document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+
+$('.speaker').click(function(e) {
+    e.preventDefault();
+  
+      $(this).toggleClass('mute');
+  });
